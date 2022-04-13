f0cabda1e778c15abd2f5126db040cdf027f3b71
Erfan <erfanzamirpour@gmail.com>
2022-04-14 01:30:05 +0430

    create() fixed

--- src/main/java/armor/ArmArmor.java
+++ src/main/java/armor/ArmArmor.java
@@ -1,11 +1,14 @@
 package armor;
 
+import config.ConfigLoader;
+
 public class ArmArmor extends Armor {
+
     private ArmArmor(double durability, double k) {
         super(durability, k);
     }
-    
+
     public static ArmArmor create() {
-        return new ArmArmor(-1, -1);
+        return new ArmArmor(ConfigLoader.getArmorDurability("arm"), ConfigLoader.getArmorK("arm"));
     }
 }
